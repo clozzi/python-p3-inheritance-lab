526affecb77de3f3366bd101e4d76e7549608226
Chris Lozzi <clozzi12@gmail.com>
2023-12-06 17:53:41 -0500

    all tests passing

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge=[]):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
